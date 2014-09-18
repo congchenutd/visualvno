6a0b27c0b27f329046c78d2272b974af5f96a740
chcdmj <chcdmj@b5d1c52e-cb79-e911-d157-f0909409370b>
2014-09-18 17:00:26 +0000

    + Zoom actions

--- src/com/fujitsu/us/visualvno/VNOEditor.java
+++ src/com/fujitsu/us/visualvno/VNOEditor.java
@@ -114,13 +114,8 @@ public class VNOEditor extends GraphicalEditorWithFlyoutPalette
 		                                           viewer, getActionRegistry());
 		viewer.setContextMenu(menuProvider);
 		
-//        List zoomLevels = new ArrayList(3);
-//        zoomLevels.add(ZoomManager.FIT_ALL);
-//        zoomLevels.add(ZoomManager.FIT_WIDTH);
-//        zoomLevels.add(ZoomManager.FIT_HEIGHT);
-//        root.getZoomManager().setZoomLevelContributions(zoomLevels);
-
-        IAction zoomIn  = new ZoomInAction (root.getZoomManager());
+		// Zoom actions
+		IAction zoomIn  = new ZoomInAction (root.getZoomManager());
         IAction zoomOut = new ZoomOutAction(root.getZoomManager());
         getActionRegistry().registerAction(zoomIn);
         getActionRegistry().registerAction(zoomOut);
