f070076cdd206a15e39553425fbe6c1110768417
chcdmj <chcdmj@b5d1c52e-cb79-e911-d157-f0909409370b>
2014-12-28 01:39:30 +0000

    + incorporate wang xi's change: added support for arbitrary number of policies in Sequence

--- src/org/freneticlang/netkat/Sequence.java
+++ src/org/freneticlang/netkat/Sequence.java
@@ -1,5 +1,43 @@
 package org.freneticlang.netkat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Sequence policy
+ * Based on the code of Prof. Foster and Cong Chen
+ * Change:
+ *      Added support for arbitrary number of policies
+ *      e.g., new Sequence(new PolicyA(), new PolicyB(), new PolicyC(), new PolicyD());
+ * 
+ * @author Xi Wang <xi.wang@us.fujitsu.com>
+ */
+
+public class Sequence implements Policy {
+	
+	private List<Policy> policies = new ArrayList<Policy>();
+
+    @Override
+    public String toString()
+    {
+        String result = "(";
+        for(int i = 0; i < policies.size(); ++i)
+        {
+            if(i != 0)
+                result += " ; ";
+            result += policies.get(i).toString();
+        }
+        result += ")";
+        return result;
+    }
+    
+    public Sequence(Policy... policies) {
+        for(int i = 0; i < policies.length; ++i)
+            this.policies.add(policies[i]);
+    }
+}
+
+/* Previous code dated 20141218
 public class Sequence implements Policy {
     private Policy left, right;
 
@@ -12,3 +50,4 @@ public class Sequence implements Policy {
         return ("(" + left + " ; " + right + ")");
     }
 }
+*/
